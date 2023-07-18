f1efc86861497e4a0d04c6e6d1908d40c61611c1
Ivan <ivan.korshunov.1997@gmail.com>
2023-07-19 00:41:37 +0300

    Add filtering for the Borrowings List endpoint.

--- borrowings/models.py
+++ borrowings/models.py
@@ -4,6 +4,11 @@ from django.db import models
 from books.models import Book
 
 
+class BorrowingManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(actual_return_date=None)
+
+
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
@@ -21,6 +26,9 @@ class Borrowing(models.Model):
         related_name="borrowings",
         on_delete=models.CASCADE
     )
+
+    objects = models.Manager()
+    is_active = BorrowingManager()
 
     def __str__(self):
         return f"Borrowing #{self.pk}"
--- borrowings/views.py
+++ borrowings/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, generics, mixins
+from rest_framework import generics, mixins
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ViewSetMixin
 
 from borrowings.models import Borrowing
@@ -25,12 +26,26 @@ class BorrowingViewSet(
     GenericViewSet
 ):
     queryset = Borrowing.objects.select_related("book")
-    # permission_classes = [IsAdminOrReadOnly]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         queryset = Borrowing.objects.select_related("book")
         if not self.request.user.is_staff:
             queryset = queryset.filter(user=self.request.user)
+
+        is_active = self.request.query_params.get(
+            "is_active"
+        )
+        if is_active:
+            queryset = queryset.filter(actual_return_date=None)
+
+        if self.request.user.is_staff:
+            user_id = self.request.query_params.get(
+                "user_id"
+            )
+            if user_id:
+                queryset = queryset.filter(user_id=user_id)
+
         return queryset.distinct()
 
     def get_serializer_class(self):
